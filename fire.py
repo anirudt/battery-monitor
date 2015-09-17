216371e0a500d97ef41046f68212381aa00f19d2
anirudt <anirudt@gmail.com>
2015-09-17 16:40:23 +0530

    added 100% tweak

--- fire.py
+++ fire.py
@@ -15,8 +15,11 @@ b = a[0:a.find('%')+1]
 c = int(a[0:a.find('%')])
 
 cmd2 = "echo 'Your battery percentage is falling, it is"+b+" now' | /home/anirudt/Projects/battery_monitor/test.sh"
+cmd3 = "echo 'Your battery has been fully charged. You could unplug the charger now' | /home/anirudt/Projects/battery_monitor/test.sh"
 # Only if the battery is lesser than 20%
-if(c<=20):
+if c<=20:
     os.system(cmd2)
+elif c==100:
+    os.system(cmd3)
 else:
     print "You're doing fine."
